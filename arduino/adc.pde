f606769075d67d8ab6d25b4c9525051a6de7b863
Sho Hashimoto <hashimoto@shokai.org>
2010-12-03 15:25:07 +0900

    detect weight and open browser

---
+++ arduino/adc.pde
@@ -0,0 +1,18 @@
+// read ADC
+
+int led_pin = 13;
+int ad_pin = 0;
+boolean led_stat = true;
+
+void setup(){
+  pinMode(led_pin, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(led_pin, led_stat);
+}
+
+void loop(){
+  int ad = analogRead(ad_pin);
+  Serial.println(ad);
+  led_stat = !led_stat;
+  digitalWrite(led_pin, led_stat);
+}
---
+++ controller/main.rb
@@ -0,0 +1,100 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'bundler/setup'
+require 'socket'
+require 'eventmachine'
+require 'yaml'
+require 'rainbow'
+
+$KCODE = 'u'
+calibrate = true if ARGV.first =~ /^c/i
+
+begin
+  conf = YAML::load open(File.dirname(__FILE__)+'/config.yaml')
+rescue => e
+  STDERR.puts 'config.yaml load error'.color(:red)
+  STDERR.puts e.to_s.color(:red)
+  exit 1
+end
+p conf
+unless calibrate
+  zv = conf['weights']['0']
+  p gs = conf['weights'].keys.delete_if{|k|k=='0'}.map{|k|
+    v = conf['weights'][k]
+    (zv - v)/k.to_f
+  }
+  puts g1 = gs.inject{|a,b|a+b}.to_f/gs.size # 1グラムあたりのAD値の変化
+end
+
+begin
+  s = TCPSocket.open(conf['serial_socket_gateway']['host'], conf['serial_socket_gateway']['port'].to_i)
+rescue => e
+  STDERR.puts e.to_s.color(:red)
+  STDERR.puts 'cannot connect serial-socket-gateway'.color(:red)
+end
+
+weight = 0 # sensor
+median = 0
+g = 0 # weight (g)
+weight_tmps = Array.new
+last_obj = nil
+
+EventMachine::run do
+  EventMachine::defer do
+    loop do
+      res = s.gets
+      exit unless res
+      res.chop!.strip!
+      next if res.to_s.size < 1
+      weight_tmps << res.to_i
+      if weight_tmps.size >= 100
+        weight = (weight_tmps.inject{|a,b|a+b}.to_f/weight_tmps.size)
+        median = weight_tmps.map{|i|
+          d = i-weight
+          d *= -1 if d < 0
+          d
+        }.inject{|a,b|a+b}/weight_tmps.size
+        puts "sensor : #{weight}"
+        puts "median : #{median}"
+        weight_tmps.clear
+      end
+    end
+  end
+
+  EventMachine::defer do
+    loop do
+      break if calibrate
+      g = (zv-weight)/g1
+      puts "weight : #{g} (g)".color(:green)
+      if median < 1.5 # センサ値平均がガタついてない時
+        conf['objects'].each{|k,v|
+          if v-5 < g and g < v+5 # 面倒なのでマッチのレンジを超広くした
+            if last_obj != k # 同じ物を処理しない
+              puts "detect => #{k.split(/\./).first}".color(0,0,200)
+              puts "open #{k}"
+              begin
+                puts `open #{conf['gyazo']}#{k}`
+              rescue => e
+                STDERR.puts e.color(:red)
+              end
+            end
+            last_obj = k
+          end
+        }
+        if -5 < g and g < 5 # 何も無い
+          last_upload = nil
+          puts 'detect => !empty!'.color(200,0,0)
+        end
+      end
+      sleep 1
+    end
+  end
+
+  EventMachine::defer do
+    loop do
+      s.puts gets
+    end
+  end
+end
+
