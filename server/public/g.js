70770886a3df1ea1fd593f78c14797a783f9faf0
Sho Hashimoto <hashimoto@shokai.org>
2011-01-16 20:38:57 +0900

    item edit link

--- server/public/g.js
+++ server/public/g.js
@@ -13,12 +13,15 @@ function display(){
         item = data.items[i];
         li = $('<li />').attr('id', item.id);
         a_name = $('<a />').attr('href',gyazz_url+item.name).append(item.name);
-        console.log(a_name);
         span_name = $('<span />').addClass('name').append(a_name);
-        span_mass = $('<span />').addClass('mass').append(item.mass+' (g)');
         span_name.css('font-size',50-Math.abs(g-item.mass)*5);
+        span_mass = $('<span />').addClass('mass').append(item.mass+' (g)');
+        a_edit = $('<a />').attr('href',app_root+'/item/'+item.name).append('edit');
+        span_edit = $('<span />').addClass('edit').append(a_edit);
+
         li.append(span_name);
         li.append(span_mass);
+        li.append(span_edit);
         ul.append(li);
     }
     $('div#items').html(ul)
@@ -27,7 +30,7 @@ function display(){
 function Items(){
     this.load = function(mass, on_load){
         var uri = api+'?mass='+mass
-        $.getJSON(api, function(res){
+        $.getJSON(uri, function(res){
             if(res.error == null){
                 data = res;
                 if(on_load != null) on_load();
