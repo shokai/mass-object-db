04f18d7f74d234fd0c3ecb315fb5aaf36b528089
Sho Hashimoto <hashimoto@shokai.org>
2011-01-18 04:23:55 +0900

    edit item name

--- server/public/item.js
+++ server/public/item.js
@@ -4,8 +4,37 @@ $(function(){
 })
 
 function display(){
-    $('div#item span#name').html(item.name);
-    $('div#item span#mass').html(item.mass);
+    var sp_name = $('div#item span#name').html(item.name);
+    var sp_mass = $('div#item span#mass').html(item.mass);
     var url = gyazz_url+'/'+item.name;
-    $('div#item span#url').append($('<a />').append(url).attr('href',url));
+    $('div#item span#url').html($('<a />').append(url).attr('href',url));
+
+    sp_name.live('dblclick', function(e){
+        var input = $('<input />').attr('id','name').attr('size', 30).attr('value', item.name);
+        var btn = $('<input />').attr('type', 'button').attr('value', '保存');
+        sp_name.html(input).append(btn);
+        btn.click(function(e){
+            put_item('name', $('input#name').val(), reload);
+        });
+        sp_name.die('dblclick');
+    });
+}
+
+function put_item(name, value, on_put){
+    $.ajax({
+        type : 'PUT',
+        url : app_root+'/api/item.json',
+        data : { name : value, 'id' : item.id },
+        dataType : 'json',
+        success : on_put
+    });
+}
+
+function reload(){
+    var uri = app_root+'/api/item/'+item.id+'.json'
+    $.getJSON(uri, function(res){
+        if(res.error == null) item = res;
+        display();
+    });
+
 }
