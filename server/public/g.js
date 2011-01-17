18168267caf887c06bdf518e2bb3700e604a1307
Sho Hashimoto <hashimoto@shokai.org>
2011-01-18 01:15:54 +0900

    add new item

--- server/public/g.js
+++ server/public/g.js
@@ -5,37 +5,49 @@ var mass_range = 5;
 
 $(function(){
     items.load(g, display);
+    $('div#new_item input#btn_add').click(items.add);
 })
 
 
 function display(){
-    ul = $('<ul />');
+    var ul = $('ul#items');
+    ul.html('');
     for(var i = 0; i < data.count; i++){
-        item = data.items[i];
-        li = $('<li />').attr('id', item.id);
-        a_name = $('<a />').attr('href',gyazz_url+item.name).append(item.name);
-        span_name = $('<span />').addClass('name').append(a_name);
+        var item = data.items[i];
+        var li = $('<li />').attr('id', item.id);
+        var a_name = $('<a />').attr('href',gyazz_url+item.name).append(item.name);
+        var span_name = $('<span />').addClass('name').append(a_name);
         span_name.css('font-size', (70+(mass_range-Math.abs(item.mass-g))*40)+'%');
-        span_mass = $('<span />').addClass('mass').append(item.mass+' (g)');
-        a_edit = $('<a />').attr('href',app_root+'/item/'+item.name).append('edit');
-        span_edit = $('<span />').addClass('edit').append(a_edit);
+        var span_mass = $('<span />').addClass('mass').append(item.mass+' (g)');
+        var a_edit = $('<a />').attr('href',app_root+'/item/'+item.name).append('edit');
+        var span_edit = $('<span />').addClass('edit').append(a_edit);
 
         li.append(span_name);
         li.append(span_mass);
         li.append(span_edit);
         ul.append(li);
     }
-    $('div#items').html(ul)
 }
 
 function Items(){
+    this.add = function(){
+        console.log('add');
+        var uri = app_root+'/api/item.json';
+        var post_data = {'name' : $('div#new_item input#name').val(),
+                         'mass' : g};
+        $.post(uri, post_data, function(res){
+            console.log(res);
+        }, 'json');
+    };
+    
     this.load = function(mass, on_load){
-        var uri = api+'?mass='+mass+'&range='+mass_range;
+        var uri = app_root+'/api/items.json';
+            + '?mass='+mass+'&range='+mass_range;
         $.getJSON(uri, function(res){
             if(res.error == null){
                 data = res;
                 if(on_load != null) on_load();
             }
         });
-    }
+    };
 }
