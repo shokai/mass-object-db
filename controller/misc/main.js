d4d5417dfc0dbacf8b10e59422a4bcd637d4e0f3
Sho Hashimoto <hashimoto@shokai.org>
2011-03-21 12:37:38 +0900

    ws debug console

---
+++ controller/misc/main.js
@@ -0,0 +1,38 @@
+var ws = new WebSocket("ws://localhost:8080");
+var KC = {tab:9, enter:13, left:37, up:38, right:39, down:40};
+
+ws.onmessage = function(e){
+    trace(e.data);
+};
+ws.onclose = function(){
+    log("ws closed");
+};
+ws.onopen = function(){
+    log('connected!!');
+};
+
+$(function(){
+    $('input#post').click(post);
+    $('input#message').keydown(function(e){
+        if(e.keyCode == KC.enter){
+            post();
+        }
+    });
+});
+
+function post(){
+    var mes = $('input#message').val();
+    ws.send(mes);
+    $('input#message').val("");    
+};
+
+function log(message){
+    trace("[log] "+message);
+};
+
+function trace(message){
+    var mes_div = $('<div />').html(message);
+    $('div#chat').prepend(mes_div);
+};
+
+
